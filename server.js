2e58e138089a24b31cecee653514f5fffafcadff
Nicole Marie <nicolependarvis5@gmail.com>
2023-04-10 17:11:12 -0400

    all required starter files

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
 
 
 const app = express();
-const port = 3001;
+const PORT = process.env.PORT || 3001;
 
 const connectionStringURI = `mongodb://127.0.0.1:27017`;
+
+const client = new MongoClient(connectionStringURI);
